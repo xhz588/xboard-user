c167ec4e4ad3661c4070dc664f0272dad0a5b6a8
XHZ <145253367+xhz588@users.noreply.github.com>
2024-07-24 22:00:07 +0100

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://cloudcat.top'
+window.settings = {
+  // 站点名称
+  title: '云猫机场-免费公益',
+  // 站点描述
+  description: '免费公益机场',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
